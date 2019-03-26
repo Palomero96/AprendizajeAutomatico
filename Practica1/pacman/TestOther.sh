b6205b8e8dd93ef744f6930a5c3a3a3fe17a182b
Palomero96 <100333208@alumnos.uc3m.es>
2019-03-26 19:04:47 +0100

    Script

---
+++ Practica1/pacman/TestOther.sh
@@ -0,0 +1,10 @@
+python busters.py -p BasicAgentAA -g RandomGhost -l smallClassic
+python busters.py -p BasicAgentAA -l smallClassic
+python busters.py -p BasicAgentAA -g RandomGhost -l minimaxClassic
+python busters.py -p BasicAgentAA -l minimaxClassic
+python busters.py -p BasicAgentAA -g RandomGhost -l openClassic
+python busters.py -p BasicAgentAA -l openClassic
+python busters.py -p BasicAgentAA -g RandomGhost -l trappedClassic
+python busters.py -p BasicAgentAA -l trappedClassic
+python busters.py -p BasicAgentAA -g RandomGhost -l capsuleClassic
+python busters.py -p BasicAgentAA -l capsuleClassic
---
+++ Practica1/pacman/Train.sh
@@ -0,0 +1,10 @@
+python busters.py -p BasicAgentAA -g RandomGhost -l openHunt
+python busters.py -p BasicAgentAA -l openHunt
+python busters.py -p BasicAgentAA -g RandomGhost -l newmap
+python busters.py -p BasicAgentAA -l newmap
+python busters.py -p BasicAgentAA -g RandomGhost -l oneHunt
+python busters.py -p BasicAgentAA -l oneHunt
+python busters.py -p BasicAgentAA -g RandomGhost -l smallHunt
+python busters.py -p BasicAgentAA -l smallHunt
+python busters.py -p BasicAgentAA -g RandomGhost -l classic
+python busters.py -p BasicAgentAA -l classic
